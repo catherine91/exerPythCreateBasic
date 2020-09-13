cb14a99f824df6397ed57aff47a84681f38060c0
catherine91 <70351707+catherine91@users.noreply.github.com>
2020-09-12 21:22:23 -0600

    Change made by Catherine

---
+++ calc.py
@@ -0,0 +1,4 @@
+x = int(input('This program will perform simple addition using two values. Enter first value: '))
+y = int(input('Enter second value: '))
+result = x + y
+print(str(x) + " + " + str(y) + " = " + str(result))
